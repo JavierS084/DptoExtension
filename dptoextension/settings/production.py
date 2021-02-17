76ddde00b4ee7b3dc1d701f700f7d4b0c2b52e5f
master thz <javier.sanchezb084@gmail.com>
2021-02-17 15:43:09 -0300

    cd

--- dptoextension/settings/production.py
+++ dptoextension/settings/production.py
@@ -1,9 +1,9 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['dptoextension.herokuapp.com']
+ALLOWED_HOSTS = ['https://dptoextension.herokuapp.com/']
 
 
 # Database
@@ -12,17 +12,15 @@ ALLOWED_HOSTS = ['dptoextension.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd2996bvjmj0bn9',
-        'USER': 'azqqnsvudqdrun',
-        'PASSWORD': '11aab6c5e178a17d416376d501cfdde6174d8abbb3810e0e8ee3961d8537dc3d',
-        'HOST': 'ec2-52-6-143-153.compute-1.amazonaws.com',
+        'NAME': 'dc9i7ll2vb3tgt',
+        'USER': 'dpwbyxfmxspyuv',
+        'PASSWORD': 'd77815aac68f5d215f55c314dbc371e0d3f6d5d315a7343145554f69b235ac27',
+        'HOST': 'ec2-54-225-130-212.compute-1.amazonaws.com',
         'PORT':'5432',
         
     }
 }
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
