dac6be26c004a0ab26d20b773432c9a24ed35d4e
master thz <javier.sanchezb084@gmail.com>
2021-02-17 16:25:47 -0300

    soluciones  y mejoras

--- dptoextension/settings/production.py
+++ dptoextension/settings/production.py
@@ -21,6 +21,11 @@ DATABASES = {
     }
 }
 
-STATICFILES_DIRS = (BASE_DIR, 'static')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
