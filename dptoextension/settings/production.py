626c1d263028017db79a7bc89a1050b8fa1a63c5
master thz <javier.sanchezb084@gmail.com>
2021-02-17 16:51:38 -0300

    cambios v1

--- dptoextension/settings/production.py
+++ dptoextension/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://dptoextension.herokuapp.com/']
+ALLOWED_HOSTS = ['https://extensioncyt.herokuapp.com/']
 
 
 # Database
@@ -12,10 +12,10 @@ ALLOWED_HOSTS = ['https://dptoextension.herokuapp.com/']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dc9i7ll2vb3tgt',
-        'USER': 'dpwbyxfmxspyuv',
-        'PASSWORD': 'd77815aac68f5d215f55c314dbc371e0d3f6d5d315a7343145554f69b235ac27',
-        'HOST': 'ec2-54-225-130-212.compute-1.amazonaws.com',
+        'NAME': 'dbbfssagm8arnv',
+        'USER': 'ebjqgroqjepucl',
+        'PASSWORD': '75058fd0bbef2dfcd73adb4afb2012256a914d324655619cb50176bfb88320d6',
+        'HOST': 'ec2-52-4-171-132.compute-1.amazonaws.com',
         'PORT':'5432',
         
     }
